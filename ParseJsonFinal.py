cd9677f5efc6fe24c10f6c1af1e692618b7d4934
zhangdayan1 <zhangdayan1@hotmail.com>
2024-11-26 13:56:38 -0800

    Parse down load font time and async module load time

--- ParseJsonFinal.py
+++ ParseJsonFinal.py
@@ -17,6 +17,8 @@ fields = ['dl', 'lsh', 'pss', 'ss', 'htc', 'lcss', 'lvql', 'bps', 'boi', 'propri
 extracted_data = {field: json_data['ei']['at'].get(field) or json_data['ei']['bt'].get(field) for field in fields}
 gsi_fields = ['gsi.1']
 extracted_gsi_data = {field: json_data['ei']['at']['gsi'].get(field) for field in gsi_fields}
+extracted_mdload_data = json_data["ei"]["at"]["mdload"]
+extracted_df_data = json_data["ei"]["at"]["df"]
 
 # Adjust the 'ep' and 'dl' fields to be arrays with a first element of 0, second as the original value, and third as their sum
 if 'ep' in extracted_data and isinstance(extracted_data['ep'], (int, float)):
@@ -43,6 +45,8 @@ table_data = {
     "End Time": [],
     "Elapsed Time": []
 }
+
+# Parse regular EPT metrics data
 for field, values in extracted_data.items():
     if isinstance(values, list):
         match field:
@@ -83,6 +87,8 @@ for field, values in extracted_data.items():
         table_data["Start Time"].append(values[0])
         table_data["End Time"].append(values[2])
         table_data["Elapsed Time"].append(values[1])
+
+# Parse GetSessionInfo data
 for field, values in extracted_gsi_data.items():
     if isinstance(values, list):
         match field:
@@ -99,6 +105,22 @@ for field, values in extracted_gsi_data.items():
         table_data["Start Time"].append(values[0])
         table_data["End Time"].append(values[2])
         table_data["Elapsed Time"].append(values[1])
+
+# Parse async module load data
+start_time_of_mdload = min(item[0] for item in extracted_mdload_data.values())
+end_time_of_mdload = max(sum(item) for item in extracted_mdload_data.values())
+table_data["Metric Name"].append("Async Module Loaded")
+table_data["Start Time"].append(start_time_of_mdload)
+table_data["End Time"].append(end_time_of_mdload)
+table_data["Elapsed Time"].append(end_time_of_mdload - start_time_of_mdload)
+
+# Parse download font data
+start_time_of_df = min(item[0] for item in extracted_df_data.values())
+end_time_of_df = max(sum(item) for item in extracted_df_data.values())
+table_data["Metric Name"].append("Download Fonts")
+table_data["Start Time"].append(start_time_of_df)
+table_data["End Time"].append(end_time_of_df)
+table_data["Elapsed Time"].append(end_time_of_df - start_time_of_df)
 
 df = pd.DataFrame(table_data)
 
